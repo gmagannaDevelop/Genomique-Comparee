95707f9967aa768ca0c31723d7b2cc9160deed0c
Gustavo Magaña López <28574085+gmagannaDevelop@users.noreply.github.com>
2021-11-08 18:23:32 +0100

    :beers: fixed typo, last commit message is wrong
    last commit was about moving config.toml

--- gencomp/parsing.py
+++ gencomp/parsing.py
@@ -1,4 +1,6 @@
 """
+placeholder
+wait for real docstring
 """
 from pathlib import Path
 from typing import Union, List
@@ -6,7 +8,7 @@ from typing import Union, List
 
 def parse_strains_from_filename(
     file_name: Union[str, Path], extension: str = ".bl", split_on: str = "-vs-"
-) -> List[str, str]:
+) -> List[str]:
     """Return a list containing the two compared strains from a filename"""
     if isinstance(file_name, Path):
         file_name = file_name.name
