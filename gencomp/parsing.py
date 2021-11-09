33b799e77965b834806ac29ac68614b354cbca48
Gustavo Magaña López <28574085+gmagannaDevelop@users.noreply.github.com>
2021-11-09 08:08:05 +0100

    :sparkles: Finished first version of the parser.
    .

--- gencomp/parsing.py
+++ gencomp/parsing.py
@@ -2,8 +2,23 @@
 placeholder
 wait for real docstring
 """
+
+import copy
 from pathlib import Path
 from typing import Union, List
+
+import toml
+import pandas as pd
+from tidycsv import TidyCSV as Tidy
+from gencomp.utils import ObjDict as odict
+
+__GENCOMP_DIR__ = Path(__file__).parent.absolute()
+__ASSETS_DIR__ = __GENCOMP_DIR__.joinpath("_assets")
+__CONFIG_FILE__ = __ASSETS_DIR__.joinpath("config.toml")
+
+with open(__CONFIG_FILE__, "r", encoding="utf-8") as f:
+    _config = toml.load(f, _dict=odict)
+    config = copy.deepcopy(_config)
 
 
 def parse_strains_from_filename(
@@ -15,3 +30,10 @@ def parse_strains_from_filename(
     elif not isinstance(file_name, str):
         raise TypeError(f"Expected `pathlib.Path` or `str` but got {type(file_name)}")
     return file_name.rstrip(extension).split(split_on)
+
+
+def parse_blast_to_dataframe(file: Union[str, Path]):
+    """ """
+    with Tidy(file, separator="\t") as f:
+        x = pd.read_csv(f, sep="\t", header=None, names=_config.parsing.columns)
+    return x
