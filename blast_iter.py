3473f399136ccaacf16af8dd1e5a5a084d8a5285
Gustavo Magaña López <28574085+gmagannaDevelop@users.noreply.github.com>
2021-11-09 10:18:01 +0100

    :sparkles: added parsing to dict
    It is way too huge.

--- blast_iter.py
+++ blast_iter.py
@@ -7,17 +7,26 @@ import pandas as pd
 from gencomp.parsing import (
     config,
     parse_blast_to_dataframe,
+    parse_blast_to_dict,
     parse_strains_from_filename,
 )
 from gencomp.utils import Path as path
 
-data_dir = path("ready_blast_outputs/")
+data_dir = path("blast_outputs/")
 
 
-blast = pd.DataFrame(columns=config.parsing.columns)
+# blast = pd.DataFrame(columns=config.parsing.columns)
+blast = dict()
 for file in data_dir.glob("*.bl"):
     query, target = parse_strains_from_filename(file.name)
-    tmp_df = parse_blast_to_dataframe(file)
-    tmp_df["query_strain"] = query
-    tmp_df["query_target"] = target
-    blast = pd.concat([blast, tmp_df], axis="rows")
+    # print(f"{query} : {target}")
+    if query not in blast.keys():
+        blast.update({query: dict()})
+
+    blast[query][target] = parse_blast_to_dict(file)
+
+    # with Tidy(file, separator=_config.parsing.separator) as g:
+    #   tmp_df = parse_blast_to_dataframe(file)
+    #   tmp_df["query_strain"] = query
+    #   tmp_df["query_target"] = target
+    #   blast = pd.concat([blast, tmp_df], axis="rows")
