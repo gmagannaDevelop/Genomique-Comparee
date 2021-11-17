5052967c3186d9f0a22cc545704e28489f30a681
Gustavo Magaña López <28574085+gmagannaDevelop@users.noreply.github.com>
2021-11-17 11:55:07 +0100

    :sparkles: compute core genomes of random subsets of strains
    added a set of functions to gencomp.core_genome, specially parallel_find_random_core_genomes()

--- gencomp/core_genome.py
+++ gencomp/core_genome.py
@@ -5,6 +5,7 @@ docstring
 import copy
 from typing import Tuple, List, Any, Dict, Optional, Iterable
 import numpy as np
+import multiprocessing as mp
 import networkx as nx
 
 np.random.seed(1234)
@@ -15,9 +16,6 @@ def search_bbh(
 ) -> List[Tuple[str, str]]:
     """Search bidirectional best hits"""
 
-    # strain_set = strain_set or bh.keys()
-    # strain_set = set(strain_set)
-
     if strain_set:
         bh = subset_blast_dict(bh, strain_set)
     else:
@@ -25,7 +23,6 @@ def search_bbh(
 
     bh = copy.deepcopy(bh)
     bbh = []
-
     for query_strain in strain_set:
         for target_strain in bh[query_strain].keys():
             for query_gene in bh[query_strain][target_strain].keys():
@@ -50,15 +47,74 @@ def subset_blast_dict(
     return bh_subset
 
 
+def find_count_bbh_and_core_genome(
+    bh: Dict[str, Dict[str, Dict[str, str]]], strain_set: Optional[Iterable[str]] = None
+) -> Tuple[int, int]:
+    """Helper function, returning the number of strains in `strain_set` and
+    the size of the core genome spawned by the `strain_set`.
+
+    (n_strains, n_genes_in_core_genome)
+    """
+    if strain_set:
+        bh = subset_blast_dict(bh, strain_set)
+    else:
+        strain_set = bh.keys()
+
+    n_strains = len(strain_set)
+    _bbh = search_bbh(bh, strain_set=strain_set)
+    return n_strains, len(find_core_genome(_bbh, n_species=n_strains))
+
+
 # TODO: change this to yield ?
 def find_core_genome(bbh: List[Tuple[str, str]], n_species: int) -> Any:
-    """ """
+    """Find the list of cliques (genes within the core genome)"""
     # Create an empty graph
     G = nx.Graph()
-    # Add edges formes by bbhs
+    # Add edges formed by bbhs
     G.add_edges_from(bbh)
     # Find all cliques
     C = nx.find_cliques(G)
     cliques = list(C)
 
     return [clique for clique in cliques if len(clique) == n_species]
+
+
+def find_random_core_genome_of_n(bh: Dict[str, Dict[str, Dict[str, str]]], n: int):
+    """
+    Danger! Do not call this function in parallel. np.random.choice will yield
+    the same result for each one of the parallel workers.
+    """
+    _rand_subset = set(np.random.choice(list(bh.keys()), n, replace=False))
+    _sub_bbh = search_bbh(bh, strain_set=_rand_subset)
+    return find_core_genome(_sub_bbh, n_species=n)
+
+
+def parallel_find_random_core_genomes(
+    bh: Dict[str, Dict[str, Dict[str, str]]],
+    sizes: Iterable[int],
+    batch_size: int,
+    n_threads: int,
+):
+    """For each `size` in `sizes`, compute the core genome for each one
+    of randomly selected strain subsets within the batch."""
+    _n_threads = min(n_threads, mp.cpu_count())
+
+    _strain_core_genome_size: Dict[str, List[int]] = {
+        "n_strains": [],
+        "core_genome_size": [],
+    }
+    with mp.Pool(_n_threads) as pool:
+        for size in sizes:
+            _rand_subsets = [
+                set(np.random.choice(list(bh.keys()), size, replace=False))
+                for i in range(batch_size)
+            ]
+            batch = pool.starmap(
+                find_count_bbh_and_core_genome,
+                [(bh, _rand_subset) for _rand_subset in _rand_subsets],
+            )
+            for _size, _cg_size in batch:
+                _strain_core_genome_size["n_strains"].append(_size)
+                _strain_core_genome_size["core_genome_size"].append(_cg_size)
+
+    return _strain_core_genome_size
